669d5b7cad8da57127dcb8c9d12e7c5d69f4e77e
Cyrus Dreyer <cyrus.dreyer@stonybrook.edu>
2021-12-13 15:35:48 -0500

    Specified bin

---
+++ build_rome/config.sh
@@ -0,0 +1,33 @@
+#
+# Default configuration parameters obtained from Autoconf
+#
+
+# Package information
+abinit_target="x86_64_linux_gnu4.8"
+abinit_binpkg="abinit-8.8.4_x86_64_linux_gnu4.8"
+abinit_host="x86_64-unknown-linux-gnu"
+
+# Version
+abinit_version="8.8.4"
+abinit_version_base="8.8"
+abinit_version_major="8"
+abinit_version_minor="8"
+abinit_version_micro="4"
+abinit_version_build="20211108"
+
+# Locations
+abinit_srcdir="/mnt/home/cdreyer/apps/wan_vlfrc_abinit-8.8.4/abinit-8.8.4/build_rome/.."
+abinit_builddir="/mnt/home/cdreyer/apps/wan_vlfrc_abinit-8.8.4/abinit-8.8.4/build_rome"
+abinit_distdir="abinit-8.8.4"
+
+# Installation
+abinit_prefix="/usr/local"
+abinit_bindir="/usr/local/bin"
+abinit_datdir="/usr/local/share/abinit"
+abinit_docdir="/usr/local/doc/abinit"
+abinit_incdir="/usr/local/include"
+abinit_libdir="/usr/local/lib"
+abinit_mandir="/usr/local/share/man"
+
+# Nightly builds
+nightly_timeout="@nightly_timeout@"
